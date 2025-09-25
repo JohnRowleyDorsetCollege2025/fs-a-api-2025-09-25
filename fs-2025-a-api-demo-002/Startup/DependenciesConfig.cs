86c3e3243ffedf8496f33e9615c8ca62468ef2a8
sakshi <sakshi.panchale@dorset.ie>
2025-09-25 10:50:25 +0100

    added transient

--- fs-2025-a-api-demo-002/Startup/DependenciesConfig.cs
+++ fs-2025-a-api-demo-002/Startup/DependenciesConfig.cs
@@ -1,11 +1,12 @@
-﻿namespace fs_2025_a_api_demo_002.Startup
+﻿using fs_2025_a_api_demo_002.Data;
+
+namespace fs_2025_a_api_demo_002.Startup
 {
     public static class DependenciesConfig
     {
         public static void AddDependencies(this WebApplicationBuilder builder)
         {
-            // Add your service registrations here
-            // e.g., services.AddTransient<IMyService, MyService>();
+           builder.Services.AddTransient<CourseData>();
         }
     }
 }
